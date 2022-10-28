55cde848ddd4ee284b1d79c0c4328ed4ccdb2780
unknown <bougatfa.ahmed99@gmail.com>
2022-10-28 11:38:45 +0200

    tp3

--- src/main/java/com/nadhem/produits/entities/Produit.java
+++ src/main/java/com/nadhem/produits/entities/Produit.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 @Entity
 public class Produit {
 @Id
@@ -12,6 +13,9 @@ private Long idProduit;
 private String nomProduit;
 private Double prixProduit;
 private Date dateCreation;
+
+@ManyToOne
+private Categorie categorie;
 public Produit() {
 super();
 }
